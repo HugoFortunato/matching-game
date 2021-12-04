c32bcdea4ace4473194d42e36978193e5d135b53
Hugo Fortunato <hugo.fortunato@proenem.com.br>
2021-12-03 22:36:27 -0300

    Evento click no cardfrontback

--- src/components/CardFrontBack/index.ts
+++ src/components/CardFrontBack/index.ts
@@ -1,13 +1,31 @@
-import CardGame from '../CardGame'
+import CardGame from "../CardGame";
+import "./style.css";
 
 function CardFrontBack() {
-    return /*html*/`
-        <article class="card-front-back">
-           ${CardGame()} 
-           ${CardGame("javascript", "logo do javascript")} 
-        </article>
-    `
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target
+        const $cardFrontBack = $origin.closest('.card-front-back')
+
+        // Dois métodos a seguir para verificar se a classe -active é existe ou não.
+
+        //1° método
+        // $cardFrontBack.classList.contains('-active') ? $cardFrontBack.classList.remove('-active') : $cardFrontBack.classList.add('-active')
+
+        //2° método
+        $cardFrontBack.classList.toggle('-active')
+    } 
+
+  return /*html*/ `
+    <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame("javascript", "Logo do JavaScript")}
+      </div>
+    </article>
+  `;
 }
 
-export default CardFrontBack
-
+export default CardFrontBack;
