20b121dda29b4369edf6440e5ad85c2abe8d7c7b
Hugo Fortunato <hugo.fortunato@proenem.com.br>
2021-12-08 22:07:36 -0300

    CSS componente VsPlayer

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import PlayerName from '../../components/PlayerName'
 import VsPlayer from '../../components/VsPlayer'
+import PlayerScore from '../../components/PlayerScore'
 import './style.css';
 
 function ScoreBoard() {
     return /*html*/`
         <header class="score-board">
             ${PlayerName('Player1')}
+            ${PlayerScore(3)}
             ${VsPlayer()}
+            ${PlayerScore()}
             ${PlayerName('Player2')}
         </header>
     `
